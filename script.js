fbff4e36ffec8b68918ad1a6564c58d09369219a
younuskhan1 <md.younuskhan412@gmail.com>
2024-02-20 14:52:23 +0600

    something are added

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
 
 next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
+    let items = document.querySelectorAll('.item');
+    // console.log(items);
+    const slideValue = document.querySelector(".slide");
+    // console.log(slideValue);
+    document.querySelector('.slide').appendChild(items[0]);
 })
 
 prev.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
+    let items = document.querySelectorAll('.item');
+    // console.log(items);
+    const slideValue = document.querySelector(".slide");
+    // console.log(slideValue);
     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
 })
